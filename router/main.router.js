e82ad452ad9e7206e23f070c32912f45dd826bd2
orange1337 <rost@MacBook-Pro-rost.local>
2018-09-06 15:48:01 +0300

    Hendler

--- router/main.router.js
+++ router/main.router.js
@@ -1,59 +1,66 @@
 /*
 * Created by Rost
 */
-const path 		= require('path');
-const fs   		= require('fs');
 const exec 		= require('child_process').exec;
-const secret 	= "cryptolionsDelivery1337";
+const createHandler     = require('github-webhook-handler')
+const handler           = createHandler({ path: '/delivery', secret: 'cryptolionsDelivery1337' })
 
 module.exports = function(router) {
 
-	router.post('/delivery', (req, res) => {
+        handler.on('error', function (err) {
+          console.error('Error:', err.message)
+        });
+        
+        handler.on('push', function (event) {
+                let data = event.payload;
+                if (!data || !data.hook || !data.hook.config || !data.hook.config.secret || 
+                        !data.repository || !data.repository.name){
+                        console.log("Wrong data", data);
+                        return res.send(444, "Wrong data");
+                }
 
-		let data = req.body;
+                if (data.hook.config.secret !== secret){
+                        return res.send(500, "Wrong secret", data.hook.config.secret);
+                }
 
-		if (!data || !data.hook || !data.hook.config || !data.hook.config.secret || 
-			!data.repository || !data.repository.name){
-			console.log("Wrong data", data);
-			return res.send(444, "Wrong data");
-		}
-
-		if (data.hook.config.secret !== secret){
-                        console.log(data.hook.config.secret);
-			return res.send(500, "Wrong secret", data.hook.config.secret);
-		}
-
-		if (data.repository.name === "eos-monitor"){
-			console.log('====== running delivery eos-monitor', new Date());
-        	        exec('cd ~ && ./delivery.sh eos-monitor', { maxBuffer: 1024 * 1000000 }, (error, sdtout, stderror) => {
-        	              if (error) {
-        	                return console.error(error);
-        	              }
-        	              if (stderror) {
-        	                console.error('stderror', stderror);
-        	              }
-        	              console.log('sdtout', sdtout);
+                if (data.repository.name === "eos-monitor"){
+                        console.log('====== running delivery eos-monitor', new Date());
+                        exec('cd ~ && ./delivery.sh eos-monitor', { maxBuffer: 1024 * 1000000 }, (error, sdtout, stderror) => {
+                              if (error) {
+                                return console.error(error);
+                              }
+                              if (stderror) {
+                                console.error('stderror', stderror);
+                              }
+                              console.log('sdtout', sdtout);
                               
-        	        });
+                        });
                         res.end();
-		} else if (data.repository.name === "eos-monitor-back"){ 
-			console.log('====== running delivery eos-monitor', new Date());
-        	        exec('cd ~ && ./delivery.sh eos-monitor-back', { maxBuffer: 1024 * 1000000 }, (error, sdtout, stderror) => {
-        	              if (error) {
-        	                return console.error(error);
-        	              }
-        	              if (stderror) {
-        	                console.error('stderror', stderror);
-        	              }
-        	              console.log('sdtout', sdtout);
+                } else if (data.repository.name === "eos-monitor-back"){ 
+                        console.log('====== running delivery eos-monitor', new Date());
+                        exec('cd ~ && ./delivery.sh eos-monitor-back', { maxBuffer: 1024 * 1000000 }, (error, sdtout, stderror) => {
+                              if (error) {
+                                return console.error(error);
+                              }
+                              if (stderror) {
+                                console.error('stderror', stderror);
+                              }
+                              console.log('sdtout', sdtout);
                              
-        	        });
+                        });
                         res.end();
-		} else {
-                   res.send(404, "Repo not found!");     
-                }	
-		
-	});
+                } else {
+                   res.send("Repo not found!");     
+                }       
+        });
+        
+        handler.on('issues', function (event) {
+          console.log('Received an issue event for %s action=%s: #%d %s',
+            event.payload.repository.name,
+            event.payload.action,
+            event.payload.issue.number,
+            event.payload.issue.title)
+        });
 
 // ============== END of exports 
 };
